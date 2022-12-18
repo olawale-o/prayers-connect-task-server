aad59d42f0e5c2cb42be21497cd11854e692e07f
olawale-o <omoogunolawale@gmail.com>
2022-12-19 00:09:21 +0100

    Create validation middleware for new task

---
+++ task/validation/validate-new-task.js
@@ -0,0 +1,23 @@
+const Joi = require('joi');
+const AppError = require('../../common/app-error');
+const httpStatus = require('../../common/http-status');
+
+const validateTaskBody = (schema) => (req, res, next) => {
+  const { value, error } = Joi.compile(schema).validate(req.body.task);
+
+  if (error) {
+    const errorMessage = error.details
+      .map((details) => details.message)
+      .join(', ')
+
+    return next(new AppError(
+      httpStatus.BAD_REQUEST.code,
+      errorMessage,
+    ));
+  }
+
+  Object.assign(req, value);
+  return next()
+}
+
+module.exports = validateTaskBody;
