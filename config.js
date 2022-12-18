53532a16ebb78bd62704d8befd37d5c8bed38b48
olawale-o <omoogunolawale@gmail.com>
2022-12-18 02:43:05 +0100

    Define schema for convict

--- config.js
+++ config.js
@@ -1,8 +1,36 @@
-require('dotenv').config();
-const { PORT, NODE_ENV, LOCAL_MONGODB_SINGLESET } = process.env;
+const convict = require('convict');
 
-module.exports = {
-  PORT,
-  NODE_ENV, 
-  LOCAL_MONGODB_SINGLESET
-};
+var config = convict({
+  env: {
+    doc: 'The application environment.',
+    format: ['production', 'development', 'test'],
+    default: 'development',
+    env: 'NODE_ENV'
+  },
+  port: {
+    doc: 'The port to bind.',
+    format: 'port',
+    default: 5000,
+    env: 'PORT',
+    arg: 'port'
+  },
+  db: {
+    host: {
+      doc: 'Database host name',
+      format: '*',
+      default: 'mongodb://localhost:27017/taskdb?retryWrites=true&w=majority',
+    },
+    name: {
+      doc: 'Database name',
+      format: String,
+      default: 'taskdb'
+    }
+  }
+});
+
+const env = config.get('env');
+config.loadFile('./.config/' + env + '.json');
+
+config.validate({allowed: 'strict'});
+
+module.exports = config;
