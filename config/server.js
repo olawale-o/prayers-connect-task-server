b0ad27536b45bfd47b9be977b6cd74604ee65543
olawale-o <omoogunolawale@gmail.com>
2022-12-18 23:36:54 +0100

    Use error handler

--- config/server.js
+++ config/server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const handleError = require('../common/error-handler');
 
 const corsOption = {
   origin: 'http://localhost:3000',
@@ -21,5 +22,9 @@ app.use(cors(corsOption));
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, { explorer: true }));
 
 app.use('/api/v1/task', require('../task/route'));
+
+app.use(async (err, req, res, next) => {
+  await handleError(err, res);
+})
 
 module.exports = app;
